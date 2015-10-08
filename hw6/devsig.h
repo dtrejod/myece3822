a93d71160d8cedcc5738873b6853957d0cef343d
Devin Trejo <devin.trejo@temple.edu>
2015-10-07 22:11:53 -0400

    Split function into a seperate header

---
+++ hw6/devsig.h
@@ -0,0 +1,10 @@
+# ifndef DEVSIG_H
+# define DEVSIG_H
+
+
+// Function Declaration
+//
+int parse_signal_file(int data_type, int show_line, FILE* fp);
+
+# endif
+
--- hw6/print_signals.cc
+++ hw6/print_signals.cc
@@ -1,8 +1,8 @@
 # include <stdio.h>
-# include <string.h>
 # include <getopt.h>
 # include <stdlib.h>
 # include <unistd.h>
+# include "devsig.h"
 
 /*
 // C++ Progam
@@ -12,67 +12,6 @@
 // Author: Devin Trejo
 //
 */
-
-// Function Declaration
-//
-//int parse_signal_file(int data_type, int show_line, FILE* fp);
-
-
-// Functiions
-//
-
-// Buffer size in bytes
-static const int buffer_size=1024;
-
-int parse_signal_file(int data_inter, int show_line, FILE* fp){
-    
-    // Line number tracker
-    //
-    long line_num = 0;
-     
-    // Create a buffer of size buffer_size in bits
-    //
-    
-    if (data_inter == 0){
-        short int buffer[buffer_size];
-        // Read in data in buffer_size chunks until end of file
-        //
-        while (fread(buffer, sizeof(short int), 
-                        buffer_size, fp) != 0){
-            // We parse buffer into data_size bits
-            //
-            for(int i = 0; i <= sizeof(buffer) / sizeof(short int); i++){
-                // If showline number is true then display line number
-               //fprintf(stdout, "i = %d ", i); 
-                if (show_line == 1){
-                    fprintf(stdout, "%08d:    ", line_num);
-                    line_num += 1;
-                }
-                fprintf(stdout, "%hd\n", buffer[i]);
-            }
-        }
-    }
-    else{
-        float buffer[buffer_size];
-        // Read in data in buffer_size chunks until end of file
-        //
-        while (fread(buffer, sizeof(float),
-                        buffer_size, fp) != 0){
-            // We parse buffer into data_size bits
-            // //
-            for(int i = 0; i <= sizeof(buffer) / sizeof(float); i++){
-               // If showline number is true then display line number
-               //fprintf(stdout, "i = %d ", i); 
-                if (show_line == 1){
-                    fprintf(stdout, "%08d:    ", line_num);
-                    line_num += 1;
-                }
-                fprintf(stdout, "%g\n", buffer[i]);
-            }
-        }
-    }
-    return 0;
-}
 
 // Input Arugment Structure
 //
